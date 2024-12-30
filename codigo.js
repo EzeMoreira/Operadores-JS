c8a126ab751d41d4a75004beeef77db52d756d37
EzequielMoreira <eezequielmoreira@gmail.com>
2024-12-30 09:28:28 -0300

    conexión con el codigo JS

---
+++ codigo.js
@@ -0,0 +1,11 @@
+// Los operadores de comparacion se utilizan para comparar dos expresiones y devolver un valor logico (True o false)
+
+
+//                       *** Operadores Logicos
+
+let password = "usuario100";
+let input = "contraseña";
+let result = input == password;
+console.log(result)
+
+// Aclaración, el valor que le damos a input en este caso es un ejemplo pero se coloca un valor distinto a la contraseña original 
