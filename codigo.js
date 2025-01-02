29956ae9a5a910f70163f7a32bc36fc1dfe442d1
EzequielMoreira <eezequielmoreira@gmail.com>
2025-01-02 11:32:57 -0300

    agregado de informacion HTML

--- codigo.js
+++ codigo.js
@@ -3,9 +3,13 @@
 
 //                       *** Operadores Logicos
 
-let password = "usuario100";
-let input = "contraseña";
-let result = input == password;
-console.log(result)
+// Por ejemplo, para loguearse en una cuenta 
 
+// let password = "usuario110"
+// let input = "aqui ponemos cualquier otro dato distinto a la contraseña original" //(input es lo que vamos a insertar desde la interfaz)
+// let result = input == password; 
+// console.log(result)
+
+// Entonces la aplicacion hace una comparacion diciendo, es la entrada del usuario (input) igual a la contraseña que tenemos en la base de datos?
+// Entonces como da false no nos permitiria ingresar, si es correcta directamente accedemos a la app o pagina web.
 // Aclaración, el valor que le damos a input en este caso es un ejemplo pero se coloca un valor distinto a la contraseña original 
